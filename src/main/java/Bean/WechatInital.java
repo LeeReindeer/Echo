99384f002d9c01061b75d9773c0d8b718e161ed2
LeeReindeer <danny.ds432@gmail.com>
2017-04-15 19:55:12 +0800

    get access_token and post menu

--- src/main/java/Bean/WechatInital.java
+++ src/main/java/Bean/WechatInital.java
@@ -8,13 +8,17 @@ import net.sf.json.JSONObject;
  * Created by lee on 4/9/17.
  */
 public class WechatInital {
+    //private static final String access_token="1quFT33F7YLcQbF99QGT_fNUZ4CQSaGBnpVSenF-BqfGZLfeu1ewDSaKdwC8v60TIFwz8XFXwdg9vRxB77lu5LPkUL9HkXfhpU6WEIsRMe4qQe2d3ZpPupB7uu9mTHexHVVdAAAXLL";
     public static void main(String[] args){
         try{
             WechatUtil wechatUtil=new WechatUtil();
+
             AccessToken token=wechatUtil.getAcessToken();
             String access_token=token.getAccess_token();
             int expires_in=token.getExpires_in();
             System.out.println(access_token);
+
+            //转化为JSON
             String menu= JSONObject.fromObject(wechatUtil.initMenu()).toString();
             int errcode=wechatUtil.creatMenu(menu,access_token);
             if (errcode==0){
