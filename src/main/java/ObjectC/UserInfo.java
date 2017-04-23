2aaea4382931481f2008a149dcfe3b675ec38cc3
LeeReindeer <danny.ds432@gmail.com>
2017-04-23 23:01:58 +0800

    add some fields

--- src/main/java/ObjectC/UserInfo.java
+++ src/main/java/ObjectC/UserInfo.java
@@ -14,6 +14,42 @@ public class UserInfo {
     private String privilege;
     private String unionid=null;
     private String schoolid;
+    private String email;
+    private String password;
+    private String school;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getSchoolid() {
         return schoolid;
