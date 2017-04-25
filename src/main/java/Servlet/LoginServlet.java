8fe09f6fa8b494317d4ce737a9d4e0d2d7c31a40
LeeReindeer <danny.ds432@gmail.com>
2017-04-25 20:34:14 +0800

    add  school and schoolid to cookie

--- src/main/java/Servlet/LoginServlet.java
+++ src/main/java/Servlet/LoginServlet.java
@@ -5,11 +5,9 @@ import Util.MysqlUtil;
 import Util.dbConnector;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -28,10 +26,6 @@ public class LoginServlet extends HttpServlet{
 
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-        /*request.setAttribute("UserInfo", userInfo);
-        request.getRequestDispatcher("userSetting.jsp").forward(request,response);*/
-
-
     }
 
     @Override
@@ -43,27 +37,40 @@ public class LoginServlet extends HttpServlet{
 
         String school=request.getParameter("school");
         String schoolid=request.getParameter("schoolid");
+        String  cschool= URLEncoder.encode(request.getParameter("school"),"UTF-8");
+        String cschoolid= URLEncoder.encode(request.getParameter("schoolid"),"UTF-8");
         String password=request.getParameter("password");
         HttpSession session=request.getSession();
-
-
         UserInfo userInfo=new UserInfo();
         userInfo.setSchoolid(schoolid);
         userInfo.setPassword(password);
         userInfo.setSchool(school);
+        //todo add schoolid and school to cookie
+        Cookie cSchool=new Cookie("school",cschool);
+        Cookie cSchoolid=new Cookie("schoolid",cschoolid);
+        cSchool.setMaxAge(5*365*24*60*60);
+        cSchoolid.setMaxAge(5*365*24*60*60);
+        response.addCookie(cSchool);
+        response.addCookie(cSchoolid);
         try {
             if (schoolid != null && password != null) {
                 Connection connection = dbConnector.getConnector();
                 MysqlUtil mysqlUtil = new MysqlUtil(connection);
-                //密码和学号相匹配
+                //密码和学号是否相匹配
                 if (mysqlUtil.checkUser(userInfo)) {
                     ResultSet rs = mysqlUtil.checkResult(userInfo);
                     //从数据库提取数据
                     String name = rs.getString("name");
                     userInfo.setName(name);
-                    request.setAttribute("UserInfo", userInfo);
-                    //跳转d
-                    request.getRequestDispatcher("userSetting.jsp").forward(request, response);
+                    String cname=URLEncoder.encode(name,"UTF-8");
+                    Cookie cName=new Cookie("name",cname);
+                    cName.setMaxAge(5*365*24*60*60);
+                    response.addCookie(cName);
+                    session.setAttribute("school",school);
+                    session.setAttribute("name",name);
+                    session.setAttribute("schoolid",schoolid);
+                    //request.getRequestDispatcher("profile.jsp").forward(request, response);
+                    response.sendRedirect("profile.jsp");
                 } else {
                     session.setAttribute("schoolid",schoolid);
                     session.setAttribute("message", "用户名或密码不匹配。");
