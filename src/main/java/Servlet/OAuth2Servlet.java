174da8c0809911ad0fca26623e53064de56f8030
LeeReindeer <danny.ds432@gmail.com>
2017-04-25 20:34:53 +0800

    read schoolid from  cookie

--- src/main/java/Servlet/OAuth2Servlet.java
+++ src/main/java/Servlet/OAuth2Servlet.java
@@ -7,21 +7,23 @@ import Util.WebUtil;
 import Util.dbConnector;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URLDecoder;
 import java.sql.Connection;
 
 import static Util.MysqlUtil.OPRNID;
+import static Util.MysqlUtil.SCHOOLID;
 
 /**
  * Created by lee on 4/15/17.
  */
 public class OAuth2Servlet extends HttpServlet {
 
-    //private UserInfo userInfo=null;
-    private String stateOpenid;
+    private static final String SplitField=",";
 
     @Override
     protected void doGet(HttpServletRequest request
@@ -29,18 +31,18 @@ public class OAuth2Servlet extends HttpServlet {
             throws ServletException,IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-        String action=request.getParameter("action");
         //连接数据库.
         Connection connection = dbConnector.getConnector();
         MysqlUtil mysqlUtil = new MysqlUtil(connection);
         // 用户同意授权后，能获取到code
-        /*if (action.equals("add")){
-
-            String schoolId=request.getParameter("schoolid");
-            mysqlUtil.updateUser(userInfo,1);
-            request.setAttribute("UserSchoolId",userInfo);
-        }else {*/
             String code = request.getParameter("code");
+            //todo  this method failed,delete
+        /*
+            String state=request.getParameter("state");
+            String []schoolField=state.split(SplitField);
+            String schoolid=schoolField[0];
+            String school=schoolField[1];
+            */
             if (!"authdeny".equals(code)) {
                 WebUtil webUtil = new WebUtil();
                 //通过code换取网页授权的access_token
@@ -52,31 +54,42 @@ public class OAuth2Servlet extends HttpServlet {
                 //获取用户信息
                 UserInfo userInfo = webUtil.getUserInfo(access_token, openid);
                 UserInfo userInfo1=userInfo;
-                //TODO ADD user's schoolid and school from usersetting.jsp
+                //TODO ADD user's schoolid and school from profile.jsp
+                String schoolid = null;
+                String school=null;
+                String name=null;
                 /*String schoolid=request.getParameter("schoolid");
-                String password=request.getParameter("password");
-                String school=request.getParameter("school");
+                String school=request.getParameter("school");*/
+                //get cookie
+                Cookie[]cookies=null;
+                cookies=request.getCookies();
+                if (cookies!=null){
+                    for (Cookie temp:cookies){
+                        if (temp.getName().equals("school")){
+                            school= URLDecoder.decode(temp.getValue(),"UTF-8");
+                        }
+                        if (temp.getName().equals("schoolid")){
+                            schoolid=URLDecoder.decode(temp.getValue(),"UTF-8");
+                        }
+                        if (temp.getName().equals("name")){
+                            name=URLDecoder.decode(temp.getValue(),"UTF-8");
+                        }
+                    }
+                }
                 userInfo1.setSchoolid(schoolid);
-                userInfo1.setPassword(password);
                 userInfo1.setSchool(school);
-                */
+                userInfo1.setName(name);
                 //若数据库中没有此openid
-                if (!mysqlUtil.isUserInSql(openid,OPRNID)) {
+                if (!mysqlUtil.isUserInSql(userInfo1,OPRNID)&&mysqlUtil.isUserInSql(userInfo1,SCHOOLID)) {
                     try {
-                        mysqlUtil.addUser(userInfo1);
-
+                        //UPDATE openid link to schoolId
+                        mysqlUtil.updateUser(userInfo1,OPRNID);
+                        request.setAttribute("UserInfo", userInfo1);
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
                 }
-                request.setAttribute("UserInfo", userInfo1);
             }
-
-            /*if (action.equals("deleteUser")) {
-
-            }
-
-        }*/
         request.getRequestDispatcher("message.jsp").forward(request,response);
     }
 }
