27765beabb32092dcb7f30ad14cc0d0ad02699b3
LeeReindeer <danny.ds432@gmail.com>
2017-05-05 23:37:16 +0800

    增加模板信息回复

--- src/main/java/ObjectC/Message/ModMessage/ArrivedTime.java
+++ src/main/java/ObjectC/Message/ModMessage/ArrivedTime.java
@@ -3,5 +3,5 @@ package ObjectC.Message.ModMessage;
 /**
  * Created by lee on 5/5/17.
  */
-public class ArrivedTime  extends datas {
+public class ArrivedTime  extends Datas {
 }
--- src/main/java/ObjectC/Message/ModMessage/Content.java
+++ src/main/java/ObjectC/Message/ModMessage/Content.java
@@ -3,6 +3,6 @@ package ObjectC.Message.ModMessage;
 /**
  * Created by lee on 5/5/17.
  */
-public class Content extends datas {
+public class Content extends Datas {
 
 }
